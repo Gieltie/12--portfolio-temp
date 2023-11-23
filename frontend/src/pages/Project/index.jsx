d1c4423236a2e2de533ef5726d881024253d2554
Gael Hofman <Gieltie@outlook.fr>
2023-11-23 11:35:46 +0100

    test render icons

--- frontend/src/pages/Project/index.jsx
+++ frontend/src/pages/Project/index.jsx
@@ -2,36 +2,39 @@ import { Link, Navigate, useParams } from 'react-router-dom';
 import data from '../../datas/projects.json';
 import './index.scss';
 
-import { SiRedux, SiCss3, SiReact, SiHtml5, SiJavascript, SiNodedotjs, SiSass } from "react-icons/si";
-
+import { SiRedux, SiCss3, SiReact, SiHtml5, SiJavascript, SiNodedotjs, SiSass, SiGithub } from "react-icons/si";
 
 function Project() {
   const { id } = useParams();
   const currentItem = data.find((item) => item.id === id);
-  const iconsList = [ SiRedux, SiCss3, SiReact, SiHtml5, SiJavascript, SiNodedotjs, SiSass ];
+  const iconsList = [
+    SiRedux, SiCss3, SiReact, SiHtml5, SiJavascript, SiNodedotjs, SiSass,
+  ];
 
   if (!currentItem) {
     return <Navigate to="/error" />;
   }
 
+  const { title, organisme, image, desc, screen, techno, github, descTwo } = currentItem;
+
   return (
     <>
-      <section className="intro">
+      <section className="intro portfolio">
         <h1 className="section__title section__title--project">
-          <strong>{currentItem.title}</strong>
+          <strong>{title}</strong>
         </h1>
         <p className="section__subtitle section__subtitle--project">
-          {currentItem.organisme}
+          {organisme}
         </p>
-        <img src={currentItem.image} alt="" className="project__img" />
+        <img src={image} alt="" className="project__img" />
       </section>
 
       <div className="portfolio__item--individual">
-        <p>{currentItem.desc}</p>
-        <img src={currentItem.screen} alt="" className="portfolio__item--img" />
+        <p>{desc}</p>
+        <img src={screen} alt="" className="portfolio__item--img" />
         <div>
           <ul className="techno">
-            {currentItem.techno.map((item, index) => {
+            {techno.map((item, index) => {
               const CustomTag = iconsList.find((icon) => icon.name === item);
               if (CustomTag) {
                 return <li className="techno__item" key={index}><CustomTag /></li>;
@@ -42,10 +45,10 @@ function Project() {
           </ul>
         </div>
 
-        <Link className="portfolio__links" to={currentItem.github}>
-          Github
+        <Link className="portfolio__links" to={github}>
+          <SiGithub />
         </Link>
-        <p>{currentItem.descTwo}</p>
+        <p>{descTwo}</p>
       </div>
     </>
   );
