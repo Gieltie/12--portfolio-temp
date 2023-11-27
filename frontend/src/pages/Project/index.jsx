0619c8d650f1cee6cd94ac3a9b3d03b700bd513e
Gael Hofman <Gieltie@outlook.fr>
2023-11-27 14:39:24 +0100

    portfolio item page done

--- frontend/src/pages/Project/index.jsx
+++ frontend/src/pages/Project/index.jsx
@@ -16,24 +16,18 @@ function Project() {
     <>
 
       <section className="portfolio-item">
-        <div className="portfolio-item__header">
-          <img src={currentItem.image} alt={currentItem.title + " logo"} className="portfolio-item__header--image" />
+        <div className='portfolio-item__description'>
+          <img src={currentItem.image} alt={currentItem.title + " logo"} className="portfolio-item__description--image" />
+          <p className='portfolio-item__description--text'>{currentItem.description}</p>
+          <div className="portfolio-item__description--link">
+            {currentItem.siteLink ? <Link to={currentItem.siteLink}><FaLink /></Link> : ""}
+            <Link to={currentItem.github}><FaGithub /></Link>
+          </div>
         </div>
-        <p className='portfolio-item__text'>{currentItem.description}</p>
-        <img src={currentItem.screen} alt={currentItem.title + " screen shot"} className="portfolio-item__image" />
-
         <div className="portfolio-item__tags">
           <h3 className="portfolio-item__tags--title">Technologies used:</h3>
           <div className="portfolio-item__tags--tag">
             {currentItem.tags.map((tag, id) => (<p key={id}>{tag}</p>))}
-          </div>
-        </div>
-
-        <div className="portfolio-item__links">
-          <h3 className="portfolio-item__links--title">Site links: </h3>
-          <div className="portfolio-item__links--link">
-            {currentItem.siteLink ? <Link to={currentItem.siteLink}><FaLink /></Link> : ""}
-            <Link to={currentItem.github}><FaGithub /></Link>
           </div>
         </div>
       </section>
